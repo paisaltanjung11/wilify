2b24ea2c762017d07cfe668e5b2188c094fb32b3
Paisal Tanjung <paisaltanjung1102@gmail.com>
2024-11-30 13:34:22 +0700

    Initial commit

---
+++ songdetail/play.js
@@ -0,0 +1,7 @@
+document.getElementById("play-button").addEventListener("click", function () {
+  document.getElementById("audio").play();
+});
+
+document.getElementById("pause-button").addEventListener("click", function () {
+  document.getElementById("audio").pause();
+});
